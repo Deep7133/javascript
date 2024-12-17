2bde5f9fba4e8701db995e8cdfa4c646b99b344c
Deep7133 <deepmenpara9@gmail.com>
2024-12-17 15:13:05 +0530

    Fixing submodule issue and adding files

---
+++ scriptt.js
@@ -0,0 +1,28 @@
+document.getElementById("name").textContent = "Deep Menpara";
+document.getElementById(
+  "Unen"
+).textContent = "B.Tech in Information Technology";
+
+document.getElementById(
+  "Unname"
+).textContent = "Parul University, 2022 - Present";
+
+document.getElementById("scen").textContent = "Senior Secondary Education";
+
+document.getElementById(
+  "scname"
+).textContent = "Modi school";
+
+document.getElementById("p1").textContent = Bhasya;
+
+document.getElementById(
+  "p1d"
+).textContent = ` Developed an interactive language learning platform that combines entertainment with education.
+ Enabled users to learn languages through real-world contexts such as movies and series clips.
+ Incorporated engaging tests and activities to assess and track progress in the learning journey.`;
+
+document.getElementById("p2").textContent = "SuitCraft";
+
+document.getElementById(
+  "p2d"
+).textContent = Designed a platform to streamline the custom suit creation process from material selection to delivery. Implemented features for fabric browsing, tailor selection, and measurement options. Developed an order workflow that connects customers with tailors and manages delivery logistics.;
